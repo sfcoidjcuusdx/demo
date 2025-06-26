2b8075762007f4b55aa079222517564b2b448800
kabirk91308 <lvanbeethoven1216@gmail.com>
2025-06-26 15:57:33 -0500

    Create LoopLessMeantimeApp.swift

---
+++ LoopLessMeantimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LooplessMeantimeApp.swift
+//  LooplessMeantime
+//
+//  Created by rafiq kutty on 6/25/25.
+//
+
+import SwiftUI
+
+@main
+struct LooplessMeantimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
